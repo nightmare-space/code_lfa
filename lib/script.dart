d97284ee3e90b6b746bff8b5e1c83db5a48f0208
nightmare <mengyanshou@gmail.com>
2025-08-23 19:57:31 +0800

    Optimize all dart code
    - User new progress logic'
    - I18n
    - Add progress description
    - User Manjaro terminal theme

--- lib/script.dart
+++ lib/script.dart
@@ -1,32 +1,50 @@
-// code-server版本号
 import 'package:global_repository/global_repository.dart';
-
 import 'config.dart';
+import 'generated/l10n.dart';
 
-String version = '';
-// proot distro 路径
+// proot distro，ubuntu path
 String prootDistroPath = '${RuntimeEnvir.usrPath}/var/lib/proot-distro';
-// ubuntu 路径
 String ubuntuPath = '$prootDistroPath/installed-rootfs/ubuntu';
+String ubuntuName = Config.ubuntuFileName.replaceAll(RegExp('-pd.*'), '');
 
-String function = '''
-export UBUNTU_PATH=$ubuntuPath
-export UBUNTU=${Config.ubuntu}
-export CSPORT=${Config.port}
-export CSVERSION=${Config.defaultCodeServerVersion}
+String common = '''
 export TMPDIR=${RuntimeEnvir.tmpPath}
 export BIN=${RuntimeEnvir.binPath}
-
+export UBUNTU_PATH=$ubuntuPath
+export UBUNTU=${Config.ubuntuFileName}
+export UBUNTU_NAME=$ubuntuName
+export CSPORT=${Config.port}
+export CSVERSION=${Config.codeServerVersion}
+export L_NOT_INSTALLED=${S.current.uninstalled}
+export L_INSTALLING=${S.current.installing}
+export L_INSTALLED=${S.current.installed}
+clear_lines(){
+  printf "\\033[1A" # Move cursor up one line
+  printf "\\033[K"  # Clear the line
+  printf "\\033[1A" # Move cursor up one line
+  printf "\\033[K"  # Clear the line
+}
 progress_echo(){
-  echo -e "\\033[31m\$@\\033[0m"
+  echo -e "\\033[31m- \$@\\033[0m"
+  echo "\$@" > "\$TMPDIR/progress_des"
+}
+bump_progress(){
+  current=0
+  if [ -f "\$TMPDIR/progress" ]; then
+    current=\$(cat "\$TMPDIR/progress" 2>/dev/null || echo 0)
+  fi
+  next=\$((current + 1))
+  printf "\$next" > "\$TMPDIR/progress"
 }
 ''';
 
-/// 安装ubuntu的shell
-String functions = r'''
+// 切换到清华源
+// Switch to Tsinghua source
+String changeUbuntuNobleSource = r'''
 change_ubuntu_source(){
   cat <<EOF > $UBUNTU_PATH/etc/apt/sources.list
 # 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释
+# Defaultly commented out source mirrors to speed up apt update, uncomment if needed
 deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu-ports/ noble main restricted universe multiverse
 # deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu-ports/ noble main restricted universe multiverse
 deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu-ports/ noble-updates main restricted universe multiverse
@@ -35,51 +53,20 @@ deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu-ports/ noble-backports main restr
 # deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu-ports/ noble-backports main restricted universe multiverse
 
 # 以下安全更新软件源包含了官方源与镜像站配置，如有需要可自行修改注释切换
+# The following security update software sources include both official and mirror configurations, modify comments to switch if needed
 # deb http://ports.ubuntu.com/ubuntu-ports/ noble-security main restricted universe multiverse
 # deb-src http://ports.ubuntu.com/ubuntu-ports/ noble-security main restricted universe multiverse
 
 # 预发布软件源，不建议启用
+# The following pre-release software sources are not recommended to be enabled
 # deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu-ports/ noble-proposed main restricted universe multiverse
 # # deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu-ports/ noble-proposed main restricted universe multiverse
 EOF
 }
-change_jammy(){
-  cat <<EOF > $UBUNTU_PATH/etc/apt/sources.list
-# 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释
-deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu-ports/ jammy main restricted universe multiverse
-# deb-src http://mirrors.tuna.tsinghua.edu.cn/ubuntu-ports/ jammy main restricted universe multiverse
-deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu-ports/ jammy-updates main restricted universe multiverse
-# deb-src http://mirrors.tuna.tsinghua.edu.cn/ubuntu-ports/ jammy-updates main restricted universe multiverse
-deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu-ports/ jammy-backports main restricted universe multiverse
-# deb-src http://mirrors.tuna.tsinghua.edu.cn/ubuntu-ports/ jammy-backports main restricted universe multiverse
+''';
 
-# 以下安全更新软件源包含了官方源与镜像站配置，如有需要可自行修改注释切换
-# deb http://ports.ubuntu.com/ubuntu-ports/ jammy-security main restricted universe multiverse
-# deb-src http://ports.ubuntu.com/ubuntu-ports/ jammy-security main restricted universe multiverse
-
-# 预发布软件源，不建议启用
-# deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu-ports/ jammy-proposed main restricted universe multiverse
-# # deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu-ports/ jammy-proposed main restricted universe multiverse
-EOF
-}
-change_ubuntu_source_ali(){
-  cat <<EOF > $UBUNTU_PATH/etc/apt/sources.list
-deb https://mirrors.aliyun.com/ubuntu/ noble main restricted universe multiverse
-deb-src https://mirrors.aliyun.com/ubuntu/ noble main restricted universe multiverse
- 
-deb https://mirrors.aliyun.com/ubuntu/ noble-security main restricted universe multiverse
-deb-src https://mirrors.aliyun.com/ubuntu/ noble-security main restricted universe multiverse
- 
-deb https://mirrors.aliyun.com/ubuntu/ noble-updates main restricted universe multiverse
-deb-src https://mirrors.aliyun.com/ubuntu/ noble-updates main restricted universe multiverse
- 
-# deb https://mirrors.aliyun.com/ubuntu/ noble-proposed main restricted universe multiverse
-# deb-src https://mirrors.aliyun.com/ubuntu/ noble-proposed main restricted universe multiverse
- 
-deb https://mirrors.aliyun.com/ubuntu/ noble-backports main restricted universe multiverse
-deb-src https://mirrors.aliyun.com/ubuntu/ noble-backports main restricted universe multiverse
-EOF
-}
+/// 安装ubuntu的shell
+String genCodeConfig = r'''
 gen_code_server_config(){
   mkdir -p $UBUNTU_PATH/root/.config/code-server 2>/dev/null
   echo "
@@ -89,33 +76,19 @@ gen_code_server_config(){
   cert: false
   " > $UBUNTU_PATH/root/.config/code-server/config.yaml
 }
+''';
 
-remove_old_ubuntu(){
-  progress_echo "- 移除旧版Ubuntu"
-}
-
-install_proot_distro(){
-  proot_distro_path=`which proot-distro`
-  if [ -z "$proot_distro_path" ]; then
-    progress_echo "- proot-distro 未安装, 安装中..."
-    cd ~
-    busybox unzip proot-distro.zip -d proot-distro
-    cd ~/proot-distro
-    bash ./install.sh
-  else
-    progress_echo "- proot-distro 已安装"
-  fi
-}
-
+String installUbuntu = r'''
 install_ubuntu(){
   mkdir -p $UBUNTU_PATH 2>/dev/null
   if [ -z "$(ls -A $UBUNTU_PATH)" ]; then
-    progress_echo "- Ubuntu 未安装, 安装中..."
+    progress_echo "Ubuntu $L_NOT_INSTALLED, $L_INSTALLING..."
+    ls ~/$UBUNTU
     busybox tar xvf ~/$UBUNTU -C $UBUNTU_PATH/ | while read line; do
       # echo -ne "\033[2K\0337\r$line\0338"
-      echo $line
+      echo -ne "\033[2K\r$line"
     done
-    UBUNTU_NAME='ubuntu-jammy-aarch64'
+    echo
     mv $UBUNTU_PATH/$UBUNTU_NAME/* $UBUNTU_PATH/
     rm -rf $UBUNTU_PATH/$UBUNTU_NAME
     echo 'export PATH=/opt/code-server-$CSVERSION-linux-arm64/bin:$PATH' >> $UBUNTU_PATH/root/.bashrc
@@ -123,29 +96,32 @@ install_ubuntu(){
   else
     VERSION=`cat $UBUNTU_PATH/etc/issue.net 2>/dev/null`
     # VERSION=`cat $UBUNTU_PATH/etc/issue 2>/dev/null | sed 's/\\n//g' | sed 's/\\l//g'`
-    progress_echo "- Ubuntu 已安装 -> $VERSION"
+    progress_echo "Ubuntu $L_INSTALLED -> $VERSION"
   fi
-  change_jammy
+  change_ubuntu_source
   echo 'nameserver 8.8.8.8' > $UBUNTU_PATH/etc/resolv.conf
-  # TODO 下面代码不能被反复执行
-  
 }
+''';
 
-
-install_vs_code_old(){
-  if [ ! -d "$UBUNTU_PATH/home/code-server-$CSVERSION-linux-arm64" ];then
-    cd $ubuntuPath/home
-    server_path="/sdcard/code-server-$CSVERSION-linux-arm64.tar.gz"
-    if [ ! -f "\$server_path" ];then
-      echo "没有发现外置包,请下载外置包"
-    else
-      progress_echo - 解压 Vs Code Arm64
-      tar zxfh \$server_path > /dev/null
-      cd code-server-$version-linux-arm64
-    fi
+// 安装 proot-distro 的脚本
+// install proot-distro script
+String installProotDistro = r'''
+install_proot_distro(){
+  proot_distro_path=`which proot-distro`
+  if [ -z "$proot_distro_path" ]; then
+    progress_echo "proot-distro $L_NOT_INSTALLED, $L_INSTALLING..."
+    cd ~
+    busybox unzip proot-distro.zip -d proot-distro
+    cd ~/proot-distro
+    bash ./install.sh
+  else
+    progress_echo "proot-distro $L_INSTALLED"
   fi
 }
+''';
 
+@Deprecated('Use genFixCodeServerHardLinkShell instead')
+String fixCodeServerHardLink = r'''
 fix_code_server_hard_link(){
   cd $UBUNTU_PATH/opt/code-server-$CSVERSION-linux-arm64/
   ls node_modules/argon2/build-tmp-napi-v3/Release
@@ -161,53 +137,64 @@ fix_code_server_hard_link(){
   cp lib/vscode/node_modules/@vscode/spdlog/build/Release/spdlog.node lib/vscode/node_modules/@vscode/spdlog/build/Release/obj.target/spdlog.node
   cp lib/vscode/node_modules/@vscode/deviceid/build/Release/windows.node lib/vscode/node_modules/@vscode/deviceid/build/Release/obj.target/windows.node
 }
+''';
 
+String genFixCodeServerHardLinkShell(Map<String, String> map) {
+  final buf = StringBuffer();
+  buf.writeln(r'fix_code_server_hard_link(){');
+  buf.writeln(r'  cd $UBUNTU_PATH/opt');
+  map.forEach((key, value) {
+    buf.writeln('  cp $value $key');
+  });
+  buf.writeln('}');
+  return buf.toString();
+}
+
+// TODO(Lin): 用 ESC 7 8 来实现，不然在手机上仍然会打印出很多行
+String installVSCodeServer = r'''
 install_vs_code(){
   if [ ! -d "$UBUNTU_PATH/opt/code-server-$CSVERSION-linux-arm64" ];then
     tar zxfh $TMPDIR/code-server-$CSVERSION-linux-arm64.tar.gz -C $UBUNTU_PATH/opt | while read line; do
-      # echo -ne "\033[2K\0337\r$line\0338"
-      echo $line
+      echo -ne "\033[2K\r$line"
     done
     # progress_echo "pwd: `pwd`"
     fix_code_server_hard_link
     # progress_echo "pwd: `pwd`"
+  else
+    progress_echo "Code Server $L_INSTALLED"
   fi
 }
-
-login_ubuntu(){
-  bash $BIN/proot-distro login --bind /storage/emulated/0:/storage/emulated/0/ ubuntu  -- /opt/code-server-$CSVERSION-linux-arm64/bin/code-server
-}
-
 ''';
 
-/// 启动 vs code 的shell
-String startVsCodeScript = '''
-$function
-$functions
+String loginUbuntu = r'''
+login_ubuntu(){
+  bash $BIN/proot-distro login --bind /storage/emulated/0:/sdcard/ ubuntu --isolated  -- /opt/code-server-$CSVERSION-linux-arm64/bin/code-server
+}
+''';
 
-clear_lines(){
-  printf "\\033[1A" # Move cursor up one line
-  printf "\\033[K"  # Clear the line
-  printf "\\033[1A" # Move cursor up one line
-  printf "\\033[K"  # Clear the line
-}
-update_progress(){
-  printf \$1 > \$TMPDIR/progress
-}
+String commonScript = '''
+$common
+$changeUbuntuNobleSource
+$installVSCodeServer
+$genCodeConfig
+$installUbuntu
+$loginUbuntu
+$installProotDistro
+clear_lines
 start_vs_code(){
-  clear_lines
   install_proot_distro
+  # return
   sleep 1
-  update_progress 7
+  bump_progress
   install_ubuntu
   sleep 1
-  update_progress 8
+  bump_progress
   install_vs_code
   sleep 1
-  update_progress 9
+  bump_progress
   gen_code_server_config
   sleep 1
-  update_progress 10
+  bump_progress
   login_ubuntu
 }
 ''';
--- lib/terminal_controller.dart
+++ lib/terminal_controller.dart
@@ -1,62 +1,69 @@
 import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
-import 'package:flutter/services.dart';
+import 'dart:ui';
 import 'package:flutter_pty/flutter_pty.dart';
 import 'package:get/get.dart';
 import 'package:global_repository/global_repository.dart';
 import 'package:settings/settings.dart';
 import 'package:xterm/xterm.dart';
 import 'config.dart';
+import 'generated/l10n.dart';
 import 'script.dart';
-import 'utils/plugin_util.dart';
-import 'utils/pty_util.dart';
+import 'utils.dart';
 
-// TODO: 点击背景即可展示终端内容
-// 默认只加载进度条
 class HomeController extends GetxController {
-  Pty? pseudoTerminal;
   bool vsCodeStaring = false;
+  SettingNode privacySetting = 'privacy'.setting;
+  Pty? pseudoTerminal;
+  Size terminalSize = Size.zero;
   late Terminal terminal = Terminal(
     maxLines: 10000,
     onResize: (width, height, pixelWidth, pixelHeight) {
-      pseudoTerminal!.resize(height, width);
+      pseudoTerminal?.resize(height, width);
+      terminalSize = Size(width.toDouble(), height.toDouble());
     },
     onOutput: (data) {
-      pseudoTerminal!.writeString(data);
+      pseudoTerminal?.writeString(data);
     },
   );
   bool webviewHasOpen = false;
 
-  double progress = 0.0;
-  double step = 11;
   String lastLine = '';
-  File file = File('${RuntimeEnvir.tmpPath}/progress');
+  File progressFile = File('${RuntimeEnvir.tmpPath}/progress');
+  File progressDesFile = File('${RuntimeEnvir.tmpPath}/progress_des');
+  double progress = 0.0;
+  double step = 17;
+  String currentProgress = '';
 
-  void updateProgress(int value) {
-    file.writeAsString('$value');
+  // 进度 +1
+  // Progress +1
+  void bumpProgress() {
+    try {
+      int current = 0;
+      if (progressFile.existsSync()) {
+        final content = progressFile.readAsStringSync().trim();
+        if (content.isNotEmpty) {
+          current = int.tryParse(content) ?? 0;
+        }
+      } else {
+        progressFile.createSync(recursive: true);
+      }
+      progressFile.writeAsStringSync('${current + 1}');
+    } catch (e) {
+      progressFile.writeAsStringSync('1');
+    }
     update();
   }
 
-  /// 监听输出，当输出中包含vscode启动成功的标志时，启动vscode
+  // 监听输出，当输出中包含启动成功的标志时，启动 Code Server
+  // Listen for output and start the Code Server when the success flag is detected
   Future<void> vsCodeStartWhenSuccessBind() async {
-    terminal.writeProgress('监听VS Code启动状态以跳转Web View...');
-    // WebView.platform = SurfaceAndroidWebView();
+    terminal.writeProgress('${S.current.listen_vscode_start}...');
     final Completer completer = Completer();
-    pseudoTerminal!.output.cast<List<int>>().transform(const Utf8Decoder(allowMalformed: true)).listen((event) async {
-      // final List<String> list = event.split(RegExp('\x0d|\x0a'));
-      // final String lastLine = list.last.trim();
-      // if (lastLine.startsWith(RegExp('dart_dio'))) {
-      //   String data = event.replaceAll(RegExp('dart_dio.*'), '');
-      //   terminal.write(data);
-      //   HttpHandler.handDownload(
-      //     controller: terminal,
-      //     cmdLine: list.last,
-      //   );
-      //   return;
-      // }
+    Utf8Decoder decoder = const Utf8Decoder(allowMalformed: true);
+    pseudoTerminal!.output.cast<List<int>>().transform(decoder).listen((event) async {
       lastLine = event.trim().split(RegExp('\x0d|\x0a')).last;
-      update();
       if (event.contains('http://0.0.0.0:${Config.port}')) {
         Log.e(event);
         if (!completer.isCompleted) {
@@ -65,43 +72,28 @@ class HomeController extends GetxController {
       }
       if (event.contains('already')) {
         Log.e(event);
-        // if (!completer.isCompleted) {
-        //   completer.complete();
-        // }
+        if (!completer.isCompleted) {
+          completer.complete();
+        }
       }
-
       terminal.write(event);
-      // event.split('').forEach((element) {
-      //   terminal.write(event);
-      // });
     });
     await completer.future;
+    bumpProgress();
     await Future.delayed(const Duration(milliseconds: 100));
     webviewHasOpen = true;
-    // 用url_launcher打开浏览器
-    updateProgress(11);
-    update();
-    PluginUtil.openWebView();
+    openWebView();
     Future.delayed(const Duration(milliseconds: 2000), () {
       vsCodeStaring = false;
-      SystemChrome.setEnabledSystemUIMode(
-        SystemUiMode.manual,
-        overlays: [],
-      );
       update();
     });
   }
 
+  // 初始化环境，将动态库中的文件链接到数据目录
+  // Init environment and link files from the dynamic library to the data directory
   Future<void> initEnvir() async {
-    List<String> androidFiles = [
-      'libbash.so',
-      'libbusybox.so',
-      'liblibtalloc.so.2.so',
-      'libloader.so',
-      'libproot.so',
-      'libsudo.so',
-    ];
-    String libPath = await PluginUtil.getLibPath();
+    List<String> androidFiles = ['libbash.so', 'libbusybox.so', 'liblibtalloc.so.2.so', 'libloader.so', 'libproot.so', 'libsudo.so'];
+    String libPath = await getLibPath();
     Log.i('libPath -> $libPath');
 
     for (int i = 0; i < androidFiles.length; i++) {
@@ -133,11 +125,14 @@ class HomeController extends GetxController {
     }
   }
 
+  // 同步当前进度
+  // Sync the current progress
   void syncProgress() {
-    file.createSync(recursive: true);
-    file.watch(events: FileSystemEvent.all).listen((event) async {
+    progressFile.createSync(recursive: true);
+    progressFile.writeAsStringSync('0');
+    progressFile.watch(events: FileSystemEvent.all).listen((event) async {
       if (event.type == FileSystemEvent.modify) {
-        String content = await file.readAsString();
+        String content = await progressFile.readAsString();
         Log.e('content -> $content');
         if (content.isEmpty) {
           return;
@@ -145,43 +140,26 @@ class HomeController extends GetxController {
         progress = int.parse(content) / step;
         Log.e('progress -> $progress');
         update();
-        // terminal.writeProgress(content);
       }
     });
-    updateProgress(0);
+    progressDesFile.createSync(recursive: true);
+    progressDesFile.writeAsStringSync('');
+    progressDesFile.watch(events: FileSystemEvent.all).listen((event) async {
+      if (event.type == FileSystemEvent.modify) {
+        String content = await progressDesFile.readAsString();
+        currentProgress = content;
+        update();
+      }
+    });
   }
 
+  // 创建 busybox 的软连接，来确保 proot-distro 会用到的命令正常运行
+  // create busybox symlinks, to ensure proot-distro can use the commands normally
   void createBusyboxLink() {
     try {
-      // 创建 ${RuntimeEnvir.binPath}/busybox 到 ${RuntimeEnvir.binPath}/xz 的软连接
       List<String> links = [
-        'xz',
-        'realpath',
-        'basename',
-        'awk',
-        'bzip2',
-        'cat',
-        'chmod',
-        'cp',
-        'curl',
-        'cut',
-        'du',
-        'file',
-        'find',
-        'grep',
-        'gzip',
-        // head id lscpu mkdir proot rm sed tar xargs xz
-        'head',
-        'id',
-        'lscpu',
-        'mkdir',
-        'rm',
-        'sed',
-        'tar',
-        'xargs',
-        'xz',
-        'uname',
-        'stat',
+        ...['awk', 'ash', 'basename', 'bzip2', 'curl', 'cp', 'chmod', 'cut', 'cat', 'du', 'dd', 'find', 'grep', 'gzip'],
+        ...['hexdump', 'head', 'id', 'lscpu', 'mkdir', 'realpath', 'rm', 'sed', 'stat', 'sh', 'tr', 'tar', 'uname', 'xargs', 'xz', 'xxd']
       ];
 
       for (String linkName in links) {
@@ -190,12 +168,14 @@ class HomeController extends GetxController {
           link.createSync('${RuntimeEnvir.binPath}/busybox');
         }
       }
+      Link link = Link('${RuntimeEnvir.binPath}/file');
+      link.createSync('/system/bin/file');
     } catch (e) {
-      Log.e('创建软连接失败 -> $e');
+      Log.e('Create link failed -> $e');
     }
   }
 
-  Future<void> createPtyTerm() async {
+  Future<void> loadCodeVersion() async {
     if (GetPlatform.isAndroid) {
       PermissionStatus status = await Permission.manageExternalStorage.request();
       Log.i('status -> $status');
@@ -203,11 +183,6 @@ class HomeController extends GetxController {
         return;
       }
     }
-    // 创建相关文件夹
-    Directory(RuntimeEnvir.tmpPath).createSync(recursive: true);
-    Directory(RuntimeEnvir.homePath).createSync(recursive: true);
-    Directory(RuntimeEnvir.binPath).createSync(recursive: true);
-    await initEnvir();
     File file = File('/sdcard/code_version');
     try {
       if (!file.existsSync()) {
@@ -215,70 +190,97 @@ class HomeController extends GetxController {
         file.writeAsStringSync(Config.defaultCodeServerVersion);
       }
     } catch (e) {
-      Log.e('创建文件失败 -> $e');
+      Log.e('Create code_version file failed -> $e');
     }
-    if (file.existsSync()) version = file.readAsStringSync();
-    if (version.isEmpty) {
-      version = Config.defaultCodeServerVersion;
+    if (file.existsSync()) Config.codeServerVersion = file.readAsStringSync();
+    if (Config.codeServerVersion.isEmpty) {
+      Config.codeServerVersion = Config.defaultCodeServerVersion;
     }
-    terminal.writeProgress('创建PTY终端实例...');
-    pseudoTerminal = createPTY();
-    updateProgress(1);
-    terminal.writeProgress('定义需要使用的函数...');
-    // Uint8List bytesFunctions = utf8.encode(startVsCodeScript);
-    // pseudoTerminal!.write(bytesFunctions);
-    await pseudoTerminal!.defineFunction(startVsCodeScript);
-    updateProgress(2);
-    terminal.writeProgress('当前VS Code Server版本:$version...');
-    // Directory('$prootDistroPath/dlcache').createSync(recursive: true);
+  }
 
-    terminal.writeProgress('拷贝 proot-distro 到数据目录...');
-    // proot-distro to launch ubuntu
-    await AssetsUtils.copyAssetToPath(
-      'assets/proot-distro.zip',
-      '${RuntimeEnvir.homePath}/proot-distro.zip',
+  void setProgress(String description) {
+    currentProgress = description;
+    terminal.writeProgress(currentProgress);
+  }
+
+  Future<void> loadCodeServer() async {
+    loadCodeVersion();
+    bumpProgress();
+    // 创建相关文件夹
+    // Create related folders
+    Directory(RuntimeEnvir.tmpPath).createSync(recursive: true);
+    Directory(RuntimeEnvir.homePath).createSync(recursive: true);
+    Directory(RuntimeEnvir.binPath).createSync(recursive: true);
+    bumpProgress();
+    await initEnvir();
+    bumpProgress();
+    // -
+    setProgress('${S.current.create_terminal_obj}...');
+    pseudoTerminal = createPTY(
+      rows: terminalSize.height.toInt() == 0 ? 24 : terminalSize.height.toInt(),
+      columns: terminalSize.width.toInt() == 0 ? 80 : terminalSize.width.toInt(),
     );
-    updateProgress(3);
-    terminal.writeProgress('拷贝 ubuntu 到数据目录...');
-    await AssetsUtils.copyAssetToPath(
-      'assets/${Config.ubuntu}',
-      '${RuntimeEnvir.homePath}/${Config.ubuntu}',
-    );
-    updateProgress(4);
-    terminal.writeProgress('创建 Busybox 符号链接...');
+    update();
+    pseudoTerminal?.resize(terminal.viewWidth, terminal.viewHeight);
+    bumpProgress();
+    // -
+    terminal.writeProgress('${S.current.current_code_version}:${Config.codeServerVersion}...');
+    setProgress('${S.current.copy_proot_distro}...');
+    await AssetsUtils.copyAssetToPath('assets/proot-distro.zip', '${RuntimeEnvir.homePath}/proot-distro.zip');
+    bumpProgress();
+    // -
+    setProgress('${S.current.copy_ubuntu}...');
+    await AssetsUtils.copyAssetToPath('assets/${Config.ubuntuFileName}', '${RuntimeEnvir.homePath}/${Config.ubuntuFileName}');
+    bumpProgress();
+    // -
+    setProgress('${S.current.create_busybox_symlink}...');
     createBusyboxLink();
-    updateProgress(5);
-    vsCodeStartWhenSuccessBind();
-    terminal.writeProgress('拷贝 code-server 到数据目录...');
+    bumpProgress();
+    // -
+    final tgxPath = '${RuntimeEnvir.tmpPath}/code-server-${Config.codeServerVersion}-linux-arm64.tar.gz';
+    setProgress('${S.current.gen_script}...');
+    Map<String, String> hardLinks = await getHardLinkMap(tgxPath);
+    String fixHardLinkShell = genFixCodeServerHardLinkShell(hardLinks);
+    Log.i('fixHardLinkShell -> $fixHardLinkShell');
+    bumpProgress();
+    // -
+    setProgress('${S.current.copy_code_server} ${RuntimeEnvir.tmpPath}...');
     await AssetsUtils.copyAssetToPath(
-      'assets/code-server-${Config.defaultCodeServerVersion}-linux-arm64.tar.gz',
-      '${RuntimeEnvir.tmpPath}/code-server-${Config.defaultCodeServerVersion}-linux-arm64.tar.gz',
+      'assets/code-server-${Config.codeServerVersion}-linux-arm64.tar.gz',
+      '${RuntimeEnvir.tmpPath}/code-server-${Config.codeServerVersion}-linux-arm64.tar.gz',
     );
-    updateProgress(6);
+    bumpProgress();
+    // -
+    vsCodeStartWhenSuccessBind();
+    bumpProgress();
+    File('${RuntimeEnvir.homePath}/common.sh').writeAsStringSync('$commonScript\n$fixHardLinkShell');
+    bumpProgress();
     startVsCode(pseudoTerminal!);
   }
 
   Future<void> startVsCode(Pty pseudoTerminal) async {
     vsCodeStaring = true;
     update();
-    pseudoTerminal.writeString('start_vs_code\n');
+    pseudoTerminal.writeString('source ${RuntimeEnvir.homePath}/common.sh\nstart_vs_code\n');
+    // pseudoTerminal.writeString('bash\n');
   }
 
   @override
   void onInit() {
     super.onInit();
+    // 为 Google Play 上架做准备
+    // For Google Play
     Future.delayed(Duration.zero, () async {
-      SettingNode settingNode = 'privacy'.setting;
-      if (settingNode.get() == null) {
+      if (privacySetting.get() == null) {
         await Get.to(PrivacyAgreePage(
           onAgreeTap: () {
-            settingNode.set(true);
+            privacySetting.set(true);
             Get.back();
           },
         ));
       }
       syncProgress();
-      createPtyTerm();
+      loadCodeServer();
     });
   }
 }
