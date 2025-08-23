dbe261fa3021d38aa69cc35dbbe6eef0e8197358
nightmare <mengyanshou@gmail.com>
2025-08-23 09:38:30 +0800

    chore: update some script

--- scripts/gen_icon.sh
+++ scripts/gen_icon.sh
@@ -1,1 +1,5 @@
-sips -z 128 128 ic_launcher.png --out icon_16x16.png
+mkdir mipmap-hdpi && sips -z 72 72 icon.png --out mipmap-hdpi/ic_launcher.png
+mkdir mipmap-mdpi && sips -z 48 48 icon.png --out mipmap-mdpi/ic_launcher.png
+mkdir mipmap-xhdpi && sips -z 96 96 icon.png --out mipmap-xhdpi/ic_launcher.png
+mkdir mipmap-xxhdpi && sips -z 144 144 icon.png --out mipmap-xxhdpi/ic_launcher.png
+mkdir mipmap-xxxhdpi && sips -z 192 192 icon.png --out mipmap-xxxhdpi/ic_launcher.png
