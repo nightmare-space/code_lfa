7ab157259103adc3fb3fe80565f461462486a968
mengyanshou <mengyanshou@gmail.com>
2025-08-23 19:57:31 +0800

    Optimize all dart code
    - User new progress logic'
    - I18n
    - Add progress description
    - User Manjaro terminal theme

--- lib/terminal_page.dart
+++ lib/terminal_page.dart
@@ -1,10 +1,11 @@
-import 'package:code_lfa/utils/pty_util.dart';
+import 'package:code_lfa/utils.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:global_repository/global_repository.dart';
 import 'package:xterm/xterm.dart';
 import 'terminal_controller.dart';
-import 'xterm_theme.dart';
+import 'terminal_theme.dart';
 
 class TerminalPage extends StatefulWidget {
   const TerminalPage({super.key});
@@ -15,20 +16,18 @@ class TerminalPage extends StatefulWidget {
 
 class _TerminalPageState extends State<TerminalPage> {
   HomeController controller = Get.put(HomeController());
-  bool visible = false;
+  ManjaroTerminalTheme terminalTheme = ManjaroTerminalTheme();
+  bool visible = false || kDebugMode;
 
   @override
   Widget build(BuildContext context) {
-    return GetBuilder<HomeController>(builder: (controller) {
-      if (controller.pseudoTerminal == null) {
-        return const SizedBox();
-      }
-      return Scaffold(
-        backgroundColor: Theme.of(context).colorScheme.surface,
-        body: PopScope(
-          // FIXME:
-          onPopInvoked: (didpop) {
+    return Scaffold(
+      backgroundColor: visible ? terminalTheme.background : Theme.of(context).colorScheme.surface,
+      body: SafeArea(
+        child: PopScope(
+          onPopInvokedWithResult: (didPop, result) {
             controller.pseudoTerminal!.writeString('\x03');
+            Get.back();
           },
           canPop: true,
           child: GestureDetector(
@@ -40,61 +39,81 @@ class _TerminalPageState extends State<TerminalPage> {
             child: Stack(
               alignment: Alignment.center,
               children: [
-                Visibility(
-                  visible: visible,
-                  // IgnorePointer
-                  child: AbsorbPointer(
-                    child: TerminalView(
-                      controller.terminal,
-                      readOnly: false,
-                      backgroundOpacity: 0,
-                      theme: macTheme,
+                Padding(
+                  padding: EdgeInsets.all(8.w),
+                  child: Visibility(
+                    visible: visible,
+                    // IgnorePointer
+                    child: AbsorbPointer(
+                      absorbing: false,
+                      child: TerminalView(
+                        controller.terminal,
+                        readOnly: false,
+                        backgroundOpacity: 1,
+                        theme: ManjaroTerminalTheme(),
+                      ),
                     ),
                   ),
                 ),
                 Center(
                   child: Material(
                     borderRadius: BorderRadius.circular(12.w),
-                    // color: Theme.of(context).colorScheme.surfaceContainer,
+                    color: Theme.of(context).colorScheme.surface,
                     child: SizedBox(
                       width: 300.w,
-                      height: 60.w,
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Center(
-                            child: LoadingProgress(
-                              minRadius: 6,
-                              strokeWidth: 3,
-                              increaseRadius: 3,
-                            ),
-                          ),
-                          SizedBox(height: 12.w),
-                          Padding(
-                            padding: EdgeInsets.symmetric(horizontal: 10.w),
-                            child: Stack(
-                              children: [
-                                Container(
-                                  height: 5.w,
-                                  decoration: BoxDecoration(
-                                    color: Theme.of(context).colorScheme.primary.withOpacity(0.2),
-                                    borderRadius: BorderRadius.circular(3.w),
-                                  ),
+                      child: Padding(
+                        padding: EdgeInsets.all(12.w),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Center(
+                              child: RepaintBoundary(
+                                child: LoadingProgress(
+                                  minRadius: 6,
+                                  strokeWidth: 3,
+                                  increaseRadius: 3,
                                 ),
-                                AnimatedContainer(
-                                  duration: 300.milliseconds,
-                                  height: 5.w,
-                                  width: 300.w * controller.progress,
-                                  decoration: BoxDecoration(
-                                    color: Theme.of(context).colorScheme.primary,
-                                    borderRadius: BorderRadius.circular(3.w),
-                                  ),
-                                ),
-                              ],
+                              ),
                             ),
-                          ),
-                        ],
+                            SizedBox(height: 12.w),
+                            GetBuilder<HomeController>(builder: (controller) {
+                              return Column(
+                                children: [
+                                  Stack(
+                                    children: [
+                                      Container(
+                                        height: 5.w,
+                                        decoration: BoxDecoration(
+                                          color: Theme.of(context).colorScheme.primary.opacity02,
+                                          borderRadius: BorderRadius.circular(3.w),
+                                        ),
+                                      ),
+                                      AnimatedContainer(
+                                        duration: 300.milliseconds,
+                                        height: 5.w,
+                                        width: 300.w * controller.progress,
+                                        decoration: BoxDecoration(
+                                          color: Theme.of(context).colorScheme.primary,
+                                          borderRadius: BorderRadius.circular(3.w),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  SizedBox(height: 8.w),
+                                  Text(
+                                    controller.currentProgress.trim(),
+                                    style: TextStyle(
+                                      fontSize: 12.w,
+                                      fontWeight: FontWeight.bold,
+                                      color: Theme.of(context).colorScheme.onSurface,
+                                    ),
+                                  ),
+                                ],
+                              );
+                            }),
+                          ],
+                        ),
                       ),
                     ),
                   ),
@@ -103,7 +122,7 @@ class _TerminalPageState extends State<TerminalPage> {
             ),
           ),
         ),
-      );
-    });
+      ),
+    );
   }
 }
